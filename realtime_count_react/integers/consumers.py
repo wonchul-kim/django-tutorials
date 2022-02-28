b50f64674d23dffb8a2b4f1d5c8e0b32b9b6494b
wonchul <onedang22@gmail.com>
2022-03-01 04:15:52 +0900

    add channel layer in wsconsumer in realtime_count_react proejct

--- realtime_count_react/integers/consumers.py
+++ realtime_count_react/integers/consumers.py
@@ -48,87 +48,143 @@
 
 ##############################################################################################
 
-import json 
-from time import sleep 
-
-from channels.generic.websocket import WebsocketConsumer, AsyncWebsocketConsumer
-from asgiref.sync import async_to_sync 
-
-class WSConsumer(AsyncWebsocketConsumer):
-    def __init__(self):
-        super(WSConsumer, self).__init__()
-        self.cnt = 0
-
-    async def connect(self):
-        await self.channel_layer.group_add('count', self.channel_name)
-        await self.accept() 
-
-        await self.send(json.dumps({"message": "accepted from the server"}))
-        
-        # cnt = 0
-        # for i in range(100):
-        #     await self.send(json.dumps({'message': cnt}))
-        #     print(cnt)
-
-        #     cnt += 1
-
-        #     sleep(1) # 1s
-
-    async def disconnect(self, close_code):
-        await self.channel_layer.group_discard('count', self.channel_name)
-
-    async def receive(self, text_data):
-        text_data_json = json.loads(text_data)
-        message = text_data_json['message']
-        print(message)
-
-        if message == 'start':
-            await self.channel_layer.group_send("count", {"type": "send_data", 'text': self.cnt})
-            self.cnt += 1
-        elif message == 'stop':
-            await self.channel_layer.group_discard('count', self.channel_name)
-
-    # async def send_data(self, message):
-    #     print("send_data: ", message)
-    #     cnt = 0
-    #     for i in range(100):
-    #         await self.send(json.dumps({'message': cnt}))
-    #         print(cnt)
-
-    #         cnt += 1
-
-
-    #         sleep(1) # 1s
-
-    async def send_data(self, message):
-        message = message['text']
-        print("send_data: ", message)
-        await self.send(json.dumps({'message': message}))
-
-##############################################################################################
-
-
 # import json 
 # from time import sleep 
 
 # from channels.generic.websocket import WebsocketConsumer, AsyncWebsocketConsumer
-# from asgiref.sync import sync_to_async
+# from asgiref.sync import async_to_sync 
 
-# class WSConsumer(WebsocketConsumer):
-#     def connect(self):
-#         self.accept() 
-#         self.send(json.dumps({"message": "accepted from the server"}))
+# class WSConsumer(AsyncWebsocketConsumer):
+#     def __init__(self):
+#         super(WSConsumer, self).__init__()
+#         self.cnt = 0
+
+#     async def connect(self):
+#         await self.channel_layer.group_add('count', self.channel_name)
+#         await self.accept() 
+
+#         await self.send(json.dumps({"message": "accepted from the server"}))
         
-#         cnt = 0
-#         for i in range(100):
-#             self.send(json.dumps({'message': cnt}))
-#             print(cnt)
+#         # cnt = 0
+#         # for i in range(100):
+#         #     await self.send(json.dumps({'message': cnt}))
+#         #     print(cnt)
 
-#             cnt += 1
-#             sleep(1) # 1s
+#         #     cnt += 1
 
-#     def receive(self, text_data):
+#         #     sleep(1) # 1s
+
+#     async def disconnect(self, close_code):
+#         await self.channel_layer.group_discard('count', self.channel_name)
+
+#     async def receive(self, text_data):
 #         text_data_json = json.loads(text_data)
 #         message = text_data_json['message']
-#         print(">>>  ", message)
+#         print(message)
 
+#         if message == 'start':
+#             await self.channel_layer.group_send("count", {"type": "send_data", 'text': self.cnt})
+#             self.cnt += 1
+#         elif message == 'stop':
+#             await self.channel_layer.group_discard('count', self.channel_name)
+
+#     # async def send_data(self, message):
+#     #     print("send_data: ", message)
+#     #     cnt = 0
+#     #     for i in range(100):
+#     #         await self.send(json.dumps({'message': cnt}))
+#     #         print(cnt)
+
+#     #         cnt += 1
+
+
+#     #         sleep(1) # 1s
+
+#     async def send_data(self, message):
+#         message = message['text']
+#         print("send_data: ", message)
+#         await self.send(json.dumps({'message': message}))
+
+##############################################################################################
+
+
+import json 
+from time import sleep 
+
+from channels.generic.websocket import WebsocketConsumer, AsyncWebsocketConsumer
+from asgiref.sync import async_to_sync
+
+class WSConsumer(WebsocketConsumer):
+    def __init__(self):
+        super().__init__()
+        self._id = 0
+
+    def connect(self):
+        async_to_sync(self.channel_layer.group_add)('count_{}'.format(self._id), self.channel_name)
+        self.accept() 
+        self.send(json.dumps({"message": "accepted from the server"}))
+        
+    def receive(self, text_data):
+        text_data_json = json.loads(text_data)
+        message = text_data_json['message']
+        print(">>>  ", message)
+
+        if message == 'start':
+            async_to_sync(self.channel_layer.group_send)("count_{}".format(self._id), {"type": "send_data", "text": 0})
+        elif message == 'stop':
+            async_to_sync(self.channel_layer.group_discard)("count_{}".format(self._id), self.channel_name)
+            self._id += 1
+            async_to_sync(self.channel_layer.group_add)('count_{}'.format(self._id), self.channel_name)
+            
+    def send_data(self, data):
+        cnt = 0
+        for i in range(5):
+            self.send(json.dumps({'message': cnt}))
+            print(cnt)
+
+            cnt += 1
+            sleep(1) # 1s
+
+
+
+##############################################################################################
+
+# import json 
+# from time import sleep 
+
+# from channels.generic.websocket import AsyncWebsocketConsumer
+
+# class WSConsumer(AsyncWebsocketConsumer):
+#     def __init__(self):
+#         super(WSConsumer, self).__init__()
+#         self.cnt = 0
+
+#     async def connect(self):
+#         await self.channel_layer.group_add(
+#                         'count',
+#                         self.channel_name
+#             )
+#         await self.accept() 
+#         await self.send(json.dumps({"message": "accepted from the server"}))
+        
+#     async def disconnect(self, close_code):
+#         await self.channel_layer.group_discard(
+#                         'count',
+#                         self.channel_name
+#             )
+
+#     async def receive(self, text_data):
+#         text_data_json = json.loads(text_data)
+#         message = text_data_json['message']
+#         print(message)
+
+#         if message == 'start':
+#             await self.channel_layer.group_send("count", {"type": "send_data", 'text': self.cnt})
+#             self.cnt += 1
+#         elif message == 'stop':
+#             await self.channel_layer.group_discard('count', self.channel_name)
+
+#     async def send_data(self, message):
+#         message = message['text']
+#         print("send_data: ", message)
+#         await self.send(json.dumps({'message': message}))
--- realtime_count_react/integers/views.py
+++ realtime_count_react/integers/views.py
@@ -1,4 +1,37 @@
-# from django.shortcuts import render
+import threading 
+import time 
 
-# def index(request):
-#     return render(request, 'index.html', context={'text': 'Hello world'})
+from channels.layers import get_channel_layer
+from asgiref.sync import async_to_sync 
+
+channel_layer = get_channel_layer() # return async func.
+
+class Publisher(threading.Thread):
+    def __init__(self, reply_channel, frequency=0.5):
+        super(Publisher, self).__init__()
+        self._running = True
+        self._reply_channel = reply_channel
+        self._publish_interval = 1.0 / frequency
+        self._data = 0
+
+    def run(self):
+        while self._running:
+           self._reply_channel.send({'text': self._data})
+           self._data += 1
+           time.sleep(self._publish_interval)
+
+    def stop(self):
+        self._running = False
+
+publishers = {}
+
+def ws_connect(message):
+    message.reply_channel.send({'accept': True})
+    publisher = Publisher(reply_channel=message.reply_channel)
+    publisher.start()
+    publishers[message.reply_channel] = publisher
+
+def ws_disconnect(message):
+    publisher = publishers[message.reply_channel]
+    publisher.stop()
+    del publishers[message.reply_channel]
